785370476592e6874c814cd51c3e308acfe3dd83
dzenan <dzenan.ramic@hach.com>
2025-06-27 01:38:04 +0200

    code clean up

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const swiperConfig = {
   speed: 400,
   spaceBetween: 8,
   slidesPerView: 2.85,
-  // centeredSlides: false,
   loop: false,
   navigation: {
     nextEl: ".swiper-button-next-custom",
@@ -17,7 +16,7 @@ const swiperConfig = {
   },
   breakpoints: {
     375: {
-      slidesPerView: 1.1, // Show 1 full + 10% of next
+      slidesPerView: 1.1,
       spaceBetween: 8,
     },
     1024: {
@@ -29,63 +28,19 @@ const swiperConfig = {
 function initSwiper() {
   swiperInstance = new Swiper(".swiper-container", swiperConfig);
   console.log("Swiper initialized.");
-  // REMOVE ALL CODE BELOW THIS LINE IN THIS FUNCTION
 }
 
 function destroySwiper() {
   if (swiperInstance) {
-    // REMOVE THE EVENT LISTENER REMOVAL CODE BELOW
     swiperInstance.destroy(true, true);
     swiperInstance = null;
     console.log("Swiper destroyed.");
   }
 }
 
-// function initSwiper() {
-//   swiperInstance = new Swiper(".swiper-container", swiperConfig);
-
-//   // Add event listeners to all custom buttons
-//   document.querySelectorAll(".swiper-button-prev-custom").forEach((btn) => {
-//     btn.addEventListener("click", () => {
-//       swiperInstance.slidePrev();
-//     });
-//   });
-
-//   document.querySelectorAll(".swiper-button-next-custom").forEach((btn) => {
-//     btn.addEventListener("click", () => {
-//       swiperInstance.slideNext();
-//     });
-//   });
-
-//   console.log("Swiper initialized.");
-// }
-
-// function destroySwiper() {
-//   if (swiperInstance) {
-//     // Remove event listeners from custom buttons
-//     document.querySelectorAll(".swiper-button-prev-custom").forEach((btn) => {
-//       btn.removeEventListener("click", () => {
-//         swiperInstance.slidePrev();
-//       });
-//     });
-
-//     document.querySelectorAll(".swiper-button-next-custom").forEach((btn) => {
-//       btn.removeEventListener("click", () => {
-//         swiperInstance.slideNext();
-//       });
-//     });
-
-//     swiperInstance.destroy(true, true);
-//     swiperInstance = null;
-//     console.log("Swiper destroyed.");
-//   }
-// }
-
-// Initialize Swiper when DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
   initSwiper();
 
-  // Toggle button functionality
   document.querySelector(".swiper-toggle").addEventListener("click", () => {
     if (swiperInstance) {
       destroySwiper();
